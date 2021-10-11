5c3e0971ddcde28359090692e294ca3ef9fbd2b7
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-10-11 17:31:04 +0200

    feat: :rocket: First commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <Nuxt/>
+    <div class="nav">
+      <div class="home"></div>
+    </div>
+  </main>
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped>
+.nav {
+  background: blue;
+  height: 80px;
+  position: relative;
+}
+
+.home {
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  background: green;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <div class="icon icon"></div>
+    <div class="icon icon-1"></div>
+    <div class="icon icon-2"></div>
+    <div class="icon icon-3"></div>
+    <div class="icon icon-4"></div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+  main {
+    height: calc(100vh - 80px);
+    background: orange;
+    position: relative;
+  }
+  .icon {
+    height: 90px;
+    width: 90px;
+    border-radius: 12px;
+    background: blue;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    &-1 {
+      top: 130px;
+      background: green;
+    }
+    &-2 {
+      top: 240px;
+      background: purple;
+    }
+    &-3 {
+      top: 350px;
+      background: yellow;
+    }
+    &-4 {
+      top: 350px;
+      background: greenyellow;
+    }
+  }
+</style>
