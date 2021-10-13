55df8abbb20a7e137aa3566e4cdcd28d04a4e3d6
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-10-13 16:40:07 +0200

    feat(store): add note

--- store/index.js
+++ store/index.js
@@ -20,6 +20,13 @@ export const state = () => ({
       isOpen: false,
       logo: 'https://icon-library.com/images/command-prompt-icon/command-prompt-icon-11.jpg',
     },
+    {
+      id: 'note',
+      isVisible: false,
+      isOpen: false,
+      logo: 'https://help.apple.com/assets/5FDCE7A064869015B86C4E71/5FDCE7A164869015B86C4E78/fr_FR/c34b1b0e87e731a83161d9bb21345afc.png',
+      note: "Je n'ai toujours pas retrouver Damien... Je dois commencer à mener l'enquete ! \nje vais noter toutes mes trouvailles ici !",
+    },
   ],
 })
 
